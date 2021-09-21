9c344f3c88febe31076b5ae71d009fbd89c66951
christianwaldmann <waldmann-christian@web.de>
2021-09-21 16:41:04 +0200

    Use HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Berechnung from "./pages/Berechnung";
 import Formeln from "./pages/Formeln";
 import { MathJaxContext } from "better-react-mathjax";
