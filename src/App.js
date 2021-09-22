85bceb807f3e3e6e4fb8f0cb924879856e89898a
christianwaldmann <waldmann-christian@web.de>
2021-09-22 13:56:07 +0200

    Swap dep react-router for react-router-async to fix warning

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Berechnung from "./pages/Berechnung";
 import Formeln from "./pages/Formeln";
 import { MathJaxContext } from "better-react-mathjax";
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export default function App() {
 	return (
-		<MathJaxContext>
-			<Helmet>
-				<body className="bg-white sm:bg-gray-50" />
-			</Helmet>
-			<Router basename={process.env.PUBLIC_URL}>
-				<Switch>
-					<Route exact path={["", "/"]}>
-						<Berechnung />
-					</Route>
-					<Route exact path="/formeln">
-						<Formeln />
-					</Route>
-				</Switch>
-			</Router>
-		</MathJaxContext>
+		<HelmetProvider>
+			<MathJaxContext>
+				<Helmet>
+					<body className="bg-white sm:bg-gray-50" />
+				</Helmet>
+				<Router basename={process.env.PUBLIC_URL}>
+					<Switch>
+						<Route exact path={["", "/"]}>
+							<Berechnung />
+						</Route>
+						<Route exact path="/formeln">
+							<Formeln />
+						</Route>
+					</Switch>
+				</Router>
+			</MathJaxContext>
+		</HelmetProvider>
 	);
 }
